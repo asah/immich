894b36403e25947fb79ec47008b9457a97dcd9b9
Adam Sah <140002+asah@users.noreply.github.com>
2026-08-07 06:20:46 +0000

    add tag edit and delete actions to canonical tag page

--- web/src/routes/(user)/tag/[slug]/+page.svelte
+++ web/src/routes/(user)/tag/[slug]/+page.svelte
@@ -3,7 +3,10 @@
   import UserPageLayout from '$lib/components/layouts/UserPageLayout.svelte';
   import Timeline from '$lib/components/timeline/Timeline.svelte';
   import { assetMultiSelectManager } from '$lib/managers/asset-multi-select-manager.svelte';
+  import { getTagActions } from '$lib/services/tag.service';
+  import { TreeNode } from '$lib/utils/tree-utils';
   import { AssetVisibility } from '@immich/sdk';
+  import { t } from 'svelte-i18n';
   import type { PageData } from './$types';
 
   let { data }: { data: PageData } = $props();
@@ -11,9 +14,12 @@
     error(404, 'Tag not found');
   }
   const tag = data.tag!;
+  const tree = TreeNode.fromTags([tag]);
+  const node = tree.traverse(tag.value);
+  const { Update, Delete } = $derived(getTagActions($t, node));
 </script>
 
-<UserPageLayout title={tag.name} descriptionHtml={tag.description ?? undefined}>
+<UserPageLayout title={tag.name} descriptionHtml={tag.description ?? undefined} actions={[Update, Delete]}>
   <Timeline
     enableRouting={true}
     options={{ tagId: tag.id, visibility: AssetVisibility.Timeline, withPartners: true, withStacked: true }}
